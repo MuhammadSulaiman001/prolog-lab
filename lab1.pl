1bdf9b59a2f6938d1838170e3e4411bc90b5783e
Muhammad Sulaiman <mosulaiman001@gmail.com>
2022-04-12 23:28:08 +0300

    lab1 on the work

--- lab1.pl
+++ lab1.pl
@@ -2,27 +2,74 @@ cls :- write('\e[2J'). % clear the console
 
 /*To reload the file into prolog: Use make. */
 
-% Simple Facts
+/* 
+- Facts:
 
-john_is_cold.                          /* john is cold */
-raining.                               /* it is raining */
-john_Forgot_His_Raincoat.              /*  john forgot his raincoat */
-fred_lost_his_car_keys.                /* fred lost is car keys */
-peter_footballer.                      /* peter plays football */
+john_is_cold.
+raining.
 
-% Facts with args
-
-eats(fred,oranges).                           /* "Fred eats oranges" */
-eats(fred,t_bone_steaks).                     /* "Fred eats T-bone steaks" */
-eats(tony,apples).                            /* "Tony eats apples" */
-eats(john,apples).                            /* "John eats apples" */
-eats(john,grapefruit).                        /* "John eats grapefruit" */
-
-% Variables
+- Predicates:
 
 eats(fred,oranges).
-eats(fred,t_bone_steak).
-eats(fred,apples).
+eats(fred,t_bone_steaks).
+
+- Variables:
 
 % ?- eats(fred,FoodItem). % Use ; to investigate more solutions
+
+*/
+
+/*
+
+Cindy likes everything that Bill likes
+likes(cindy, Something):- likes(bill, Something).
+
+Caitlin likes everything that is green
+likes(caitlin, Something):- green(Something).
+
+If human(X) then mortal(X):
+moral(X) :- human(X).
+
+*/
+
+/*
+
+hates(tom , jerry).
+?- hates(tom,jerry).  true
+?-hates(tom, Who).
+?-hates(X, Y).
+*/
+
+/*
+p(a,b).
+p(b,c).
+?-P(a, X), p(X,c).
+*/
+
+/*
+parent(abo_ahmad, ahmad).
+parent(um_ahmad, ahmad).
+parent(abo_ahmad, hamida).
+male(ahmad).
+male(tarek).
+male(omar).
+female(salma).
+female(samira).
+female(hamida).
+
+father(X,Y) :- parent(X,Y), male(X).
+mother(X,Y) :- parent(X,Y), female(X).
+child(X,Y) :- parent( Y,X).
+daughter(X,Y) :- child(X,Y), female(X).
+son(X,Y) :- child(X,Y), male(X).
+sibling(X,Y) :- parent(Z,X),parent(Z,Y),X\=Y.
+sister(X,Y):-     sibling(X,Y), female(X).
+uncle (X,Y):- parent(Z,Y), brother(Z,X),
+grandfather(X,Y):- male(X),    parent(X,Z),    parent(Z,Y).
+
+ancestor(X,Y):- parent(Z,Y), ancestor(X,Z).
+ancestor(X,Y):- parent(X,Y).
+
+*/
+
 
